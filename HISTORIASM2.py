ea9217c597f4890778b6f8ef79e9f236f7223eb2
michaeljimenez16 <michaelstevenezz16@gmail.com>
2026-03-19 14:32:09 -0500

    Update and rename README.md to HISTORIASM2.py

---
+++ HISTORIASM2.py
@@ -0,0 +1,44 @@
+inventario = []
+
+def agregar_producto():
+    """Solicita datos y los guarda en un diccionario dentro de la lista."""
+    nombre = input("Nombre del producto: ")
+    precio = float(input(f"Precio de {nombre}: "))
+    cantidad = int(input(f"Cantidad de {nombre}: "))
+
+    nuevo_producto = {
+        "nombre": nombre,
+        "precio": precio,
+        "cantidad": cantidad,
+    }
+
+    inventario.append(nuevo_producto)
+    print("Producto registrado.")
+
+def mostrar_inventario():
+    """"Recorre la lista con un bucle for para mostrar los productos."""
+    if not inventario:
+        print("El inventario esta vacio.")
+    else:
+        for p in inventario:
+            print(f"Producto: {p['nombre']} | Precio: {p['precio']} | Cantidad: {p['cantidad']}") 
+
+def calcular_estadisticas():
+    """"Calcula el valor monetario total y el numero de productos."""
+    if not inventario: 
+        print("No hay datos para calcular.")
+        return
+
+    valor_total = 0
+    conteo_productos = len(inventario)
+
+    for p in inventario:
+        valor_total += p['precio'] * p['cantidad']
+
+        print(f"\nTotal de productos distintos: {conteo_productos}" )
+        print(f"Valor total del inventario: ${valor_total}")
+
+def menu_principal():
+    """"Mantiene el sistema activo y gestiona las opciones."""
+    while True: 
+        print(\n SISTEMA )
